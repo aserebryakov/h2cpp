55f646e991e51444968e0d9106750817fce03161
Alexander Serebryakov <alex.serebr@gmail.com>
2013-11-03 23:05:25 +0200

    This commit adds writting of the output to file

--- cpp_generator.py
+++ cpp_generator.py
@@ -5,6 +5,22 @@ import doxycom
 import cpp_parser
 
 tab_level = 0
+outfile = None;
+
+def file_create(filename):
+	global outfile
+	outfile = open(filename, "w")
+	
+def file_write(string):
+	global outfile
+	if outfile == None:
+		file_write("Error! File is not open!")
+
+	outfile.write(string)
+
+def file_close():
+	global outfile
+	outfile.close()
 
 def tabbing(tab_level):
 	tabs = ""
@@ -16,7 +32,7 @@ def write_method(method, class_name, tab_level):
 	return_type = cpp_parser.get_return_type(method)
 	args = cpp_parser.get_arguments(method)
 
-	print(doxycom.doxy_gen_comment(args, return_type, tab_level))
+	file_write(doxycom.doxy_gen_comment(args, return_type, tab_level))
 	method = re.sub(r";", "", method)
 	method = re.sub(return_type, "", method)
 	method = re.sub(r"\s*", "", method, 1)
@@ -35,10 +51,12 @@ def write_method(method, class_name, tab_level):
 
 	method = method + tabbing(tab_level) + "}\n\n"
 
-	print("\n" + method)
+	file_write("\n" + method)
 
 def generate_cpp(header_name):
-	cpp_hame = re.sub(r".(h|hpp)", ".cpp", header_name)
+	cpp_name = re.sub(r".(h|hpp)", ".cpp", header_name)
+	
+	file_create(cpp_name)
 
 	header_content = cpp_parser.file_read(header_name)
 	namespaces = cpp_parser.get_namespaces(header_content)
@@ -47,13 +65,15 @@ def generate_cpp(header_name):
 
 	tab_level = len(namespaces)
 
-	print("#include \"" + header_name + "\"\n\n")
+	file_write("#include \"" + header_name + "\"\n\n")
 
 	for i in range(len(namespaces)):
-		print(tabbing(i) +  "namespace " + namespaces[i] + "\n"+ tabbing(i) + "{\n")
+		file_write(tabbing(i) +  "namespace " + namespaces[i] + "\n"+ tabbing(i) + "{\n")
 
 	for method in methods:
 		write_method(method, class_name, tab_level)
 	
 	for i in range(tab_level, 0, -1):
-		print(tabbing(i - 1) + "}\n")
+		file_write(tabbing(i - 1) + "}\n")
+
+	file_close()
