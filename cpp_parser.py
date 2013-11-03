48dca64f9d506b02aec8d5e449f65293f0373fba
Alexander Serebryakov <alex.serebr@gmail.com>
2013-11-03 22:31:07 +0200

    This commit improves header parsing

--- cpp_parser.py
+++ cpp_parser.py
@@ -3,42 +3,61 @@
 import re
 
 
+def find_all(array, regex):
+	matches = []
+
+	print(array)
+	for string in array:
+		if regex.search(string) != None:
+			matches.append(string)
+
+	print(matches)
+	return matches
+
+
 def file_read(filename):
 	input_file = open(filename, "r")
 	content = input_file.read()
 	input_file.close()
-	return content
+	return content.splitlines()
+
 
 def get_namespaces(content):
 	namespaces = []
+	ns_temp = []
 	expr = re.compile("namespace\s+[a-zA-Z0-9_]+")
-	ns_temp = expr.findall(content)
+	ns_temp = find_all(content, expr)
 	for ns in ns_temp:
-		ex = re.compile("namespace\s+")
+		ex = re.compile("\s*namespace\s+")
 		ns = ex.sub("", ns)
 		namespaces.append(ns)
+
+	print(namespaces)
 	return namespaces
+
 
 def get_class(content):
 	expr = re.compile("class\s+[a-zA-Z0-9_]+")
-	class_name = expr.findall(content)
-	class_name = re.compile("class\s+").sub("", class_name[0])
+	class_name = find_all(content, expr)
+	class_name = re.compile("\s*class\s+").sub("", class_name[0])
 	return class_name
+
 
 def get_methods(content):
 	methods = []
 	expr = re.compile("[\w\s_:]+\([^)]*\)") 
-	methods_tmp = expr.findall(content)
+	methods_tmp = find_all(content, expr)
 
 	for method in methods_tmp:
-		#method.rstrip("\n\t")
-		method = re.sub(r"\s*", "", method, 1)
+		method = re.sub(r"\s+", "", method, 1)
 		print(method)
 		methods.append(method)
 
+	print("Methods\n")
 	print(methods)
 
 	return methods
+
 
 def get_arguments(method_decl):
 	arguments_list = []
@@ -48,11 +67,12 @@ def get_arguments(method_decl):
 	arg_list_tmp = re.split(", ", arg_list_tmp)
 	
 	for arg in arg_list_tmp:
-			arg = re.sub(r"^[^\s]+\s+", "", arg)
-			if arg != "":
-				arguments_list.append(arg)
+		arg = re.sub(r"^[^\s]+\s+", "", arg)
+		if arg != "":
+			arguments_list.append(arg)
 
 	return arguments_list
+
 
 def get_return_type(method_decl):
 	ret_type = re.sub(r"^\s*", "", method_decl)
